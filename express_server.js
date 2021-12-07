dba28c228a91c61dd9f71646f9eb549cf99b27a4
cdmac94 <collinmacleod79@gmail.com>
2021-12-07 01:03:54 +0000

    Finished the basic formatting and setup of the app

--- express_server.js
+++ express_server.js
@@ -13,19 +13,19 @@ app.get("/", (req, res) => {
   res.send("Hello!, How's it hanging?");
 });
 
-app.get("urls", (req, res) => {
+app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
-  res.render('"urls_index', templateVars)
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase}
-  res.render("urls_show", templateVars)
-})
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render('urls_show', templateVars);
+});
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
