550d0e7e67e76bdc77d09719ce300c21d9343f23
cdmac94 <collinmacleod79@gmail.com>
2021-12-07 01:38:22 +0000

    Allowed for a response to come from typeing into the new url feild and added a function to create a random 6 character

--- express_server.js
+++ express_server.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Inpsipired by https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+
+function generateRandomString () {
+  let tinyName = "";
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
+  let characterLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    tinyName += characters.charAt(Math.floor(Math.random() * characterLength));
+  } return tinyName;
+};
+
 
 app.set("view engine", "ejs");
 
@@ -18,8 +32,17 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get('/urls/:shortURL', (req, res) => {
@@ -27,9 +50,7 @@ app.get('/urls/:shortURL', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
