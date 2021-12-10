a9d9ecac9aa946e22bb58fb4f6f88e6b6c970018
cdmac94 <collinmacleod79@gmail.com>
2021-12-10 04:43:10 +0000

    Final commit before submitting

--- express_server.js
+++ express_server.js
@@ -4,24 +4,15 @@ const PORT = 3000;
 const bodyParser = require("body-parser");
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcryptjs');
+const findUserByEmail = require('./helpers').findUserByEmail;
+const urlsForUser = require('./helpers').urlsForUser;
+const generateRandomString = require('./helpers.js').generateRandomString;
 
 app.use(cookieSession({
   name: 'session',
   keys: ['key1']
 }));
 app.use(bodyParser.urlencoded({extended: true}));
-
-//Inpsipired by https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
-
-function generateRandomString () {
-  let tinyName = "";
-  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
-  let characterLength = characters.length;
-  for (let i = 0; i < 6; i++) {
-    tinyName += characters.charAt(Math.floor(Math.random() * characterLength));
-  } return tinyName;
-};
-
 
 app.set("view engine", "ejs");
 
@@ -35,66 +26,44 @@ const urlDatabase = {
 };
 
 
-const users = { 
+const users = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
-const findUserByEmail = (email) => {
-  for(const userId in users) {
-    const user = users[userId];
-    if(user.email === email) {
-      return user;
-    }
-  }
-  return null;
-}
-
-const urlsForUser = (id) => {
-  const userDatabase = {}
-  for (urls in urlDatabase) {
-    const userURLs = urlDatabase[urls].userId;
-    if(id === userURLs) {
-      userDatabase[urls] = urlDatabase[urls];
-    }
-  }
-  return userDatabase;
-}
-
 
 app.get("/", (req, res) => {
-  const templateVars = {user_id: req.session["user_id"]}
   res.redirect("/urls");
 });
 
 app.get("/urls", (req, res) => {
   const userId = req.session.sessionName;
   
-  const usersURLs = urlsForUser(userId);
+  const usersURLs = urlsForUser(userId, urlDatabase);
 
-  const templateVars = {  
-    user_id: userId,
+  const templateVars = {
+    userId: userId,
     urls: usersURLs
-   };
+  };
   res.render("urls_index", templateVars);
 });
 
 //login cookie
 
 app.get("/login", (req, res) => {
-  const templateVars = {user_id: req.session.sessionName};
-  if (req.session.session){
+  const templateVars = {userId: req.session.sessionName};
+  if (req.session.session) {
     res.redirect("/");
   } else {
-  res.render("urls_login", templateVars)
+    res.render("urls_login", templateVars);
   }
 });
 
@@ -104,19 +73,16 @@ app.post("/login", (req, res) => {
 
   if (!useremail || !userpass) {
     return res.status(400).send("email and password cannot be blank");
-  };
-
-  const user = findUserByEmail(useremail);
-
-  
-  console.log(userpass)
-
-  if(!user){
-    return res.status(403).send("a user with that email does not exist")
   }
 
-  if(bcrypt.compareSync(userpass, user.email)) {
-    return res.status(403).send('password does not match')
+  const user = findUserByEmail(useremail, users);
+
+  if (!user) {
+    return res.status(403).send("a user with that email does not exist");
+  }
+
+  if (bcrypt.compareSync(userpass, user.email)) {
+    return res.status(403).send('password does not match');
   }
 
   req.session.sessionName = user.email;
@@ -132,12 +98,12 @@ app.post("/logout", (req, res) => {
 //Registration page
 
 app.get("/register", (req, res) => {
-  const templateVars = {user_id: req.session.sessionName};
-  if (req.session.sessionName){
-    res.redirect("/")
+  const templateVars = {userId: req.session.sessionName};
+  if (req.session.sessionName) {
+    res.redirect("/");
   } else {
     console.log("New member registering");
-  res.render("urls_register", templateVars);
+    res.render("urls_register", templateVars);
   }
 });
 
@@ -151,23 +117,23 @@ app.post("/register", (req, res) => {
   
   if (!useremail || !userpass) {
     return res.status(400).send("email and password cannot be blank");
-  };
+  }
   
-  const user = findUserByEmail(useremail);
+  const user = findUserByEmail(useremail, users);
   
-  if(user) {
-    return res.status(400).send("a user already exists with that email")
+  if (user) {
+    return res.status(400).send("a user already exists with that email");
   }
   
   const newUserId = {
-  id: userId, 
-  email: useremail, 
-  password: encryptedPass,
+    id: userId,
+    email: useremail,
+    password: encryptedPass,
   };
 
-  users[userId] = newUserId
+  users[userId] = newUserId;
   console.log(`New user: email: ${useremail}, userpass: ${encryptedPass}, userId: ${userId}`);
-  req.session.sessionName = newUserId.email
+  req.session.sessionName = newUserId.email;
   res.redirect("/");
 });
 
@@ -175,49 +141,40 @@ app.post("/register", (req, res) => {
 //adding to database
 
 app.post("/urls", (req, res) => {
-  const templateVars = {user_id: req.session.sessionName}
-  const userId = req.session.sessionName
+  const userId = req.session.sessionName;
   
-  if(!userId) {
-  res.status(401).send("You must be logged in to make a Tiny Url");
-  return
+  if (!userId) {
+    res.status(401).send("You must be logged in to make a Tiny Url");
+    return;
   }
   console.log(`Added ${req.body.longURL} to urlDatabase`);
   let tinyUrl = generateRandomString();
   urlDatabase[tinyUrl] = { longURL: req.body.longURL, userId: req.session.sessionName};
-  console.log(urlDatabase)
   res.redirect("/urls");
 });
 
 //link to original site
 
 app.get("/u/:shortURL", (req, res) => {
-  const templateVars = {user_id: req.session.sessionName}
   if (!urlDatabase[req.params.shortURL]) {
-    return res.status(401).send("Sorry! this link does not exist");
+    return res.status(404).send("Sorry! this link does not exist");
   }
   const longURL = urlDatabase[req.params.shortURL].longURL;
-  const shortURL = urlDatabase[req.params.shortURL].userId;
 
-  console.log(shortURL)
-  console.log(longURL);
   res.redirect(longURL);
 });
 
+// delete link
+
 app.post("/urls/:shortURL/delete", (req, res) => {
-  const templateVars = { 
-    user_id: req.session["user_id"],
-    shortURL: req.params.shortURL, 
-    longURL: urlDatabase[req.params.shortURL].longURL };
-  const Id = req.session["user_id"];
+  const Id = req.session.sessionName;
   const urlUser = urlDatabase[req.params.shortURL].userId;
-  console.log(templateVars)
-  console.log(urlUser);
-  if(!Id) {
+
+  if (!Id) {
     return res.status(401).send("You must be logged in to delete URLs");
   }
 
-  if(Id !== urlUser) {
+  if (Id !== urlUser) {
     return res.status(401).send("You can only delete your own URLs");
   }
 
@@ -229,9 +186,9 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 //link to editing/view page
 
 app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = { 
-    user_id: req.session["user_id"],
-    shortURL: req.params.shortURL, 
+  const templateVars = {
+    userId: req.session.sessionName,
+    shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL].longURL
   };
 
@@ -239,14 +196,14 @@ app.get('/urls/:shortURL', (req, res) => {
   const urlUser = urlDatabase[req.params.shortURL].userId;
   
   console.log(urlUser);
-  if(!Id) {
+  if (!Id) {
     return res.status(401).send("You must be logged in to edit URLs");
   }
 
-  if(Id !== urlUser) {
+  if (Id !== urlUser) {
     return res.status(401).send("You can only edit your own URLs");
   }
-  console.log(templateVars.longURL)
+  console.log(templateVars.longURL);
   console.log(templateVars.shortURL);
   res.render('urls_show', templateVars);
 });
@@ -255,24 +212,23 @@ app.get('/urls/:shortURL', (req, res) => {
 //after pressing submit
 
 app.post("/urls/:shortURL", (req, res) => {
-  const templateVars = {user_id: req.session.sessionName};
-  const newUrl = req.body.longURL
+  const newUrl = req.body.longURL;
   urlDatabase[req.params.shortURL]['longURL'] = newUrl;
   console.log(urlDatabase);
   res.redirect("/");
 });
 
 app.get("/urls.json", (req, res) => {
-  const templateVars = {user_id: req.session.sessionName};
+  const templateVars = {userId: req.session.sessionName};
   res.json(urlDatabase, templateVars);
 });
 
 app.get("/new", (req, res) => {
-  const templateVars = {user_id: req.session.sessionName};
+  const templateVars = {userId: req.session.sessionName};
 
   const userId = req.session.sessionName;
   
-  if(!userId) {
+  if (!userId) {
     return res.status(401).send("You must be logged into make new TinyURL");
   }
 
@@ -285,4 +241,3 @@ app.get("/new", (req, res) => {
 app.listen(PORT, () => {
   console.log(`TinyApp ready too transform url's on ${PORT}!`);
 });
-
---
+++ helpers.js
@@ -0,0 +1,34 @@
+const findUserByEmail = (email, database) => {
+  for (const userId in database) {
+    const user = database[userId];
+    if (user.email === email) {
+      return user.id;
+    }
+  }
+  return null;
+};
+
+const urlsForUser = (id, database) => {
+  const userDatabase = {};
+  for (urls in database) {
+    const userURLs = database[urls].userId;
+    if (id === userURLs) {
+      userDatabase[urls] = database[urls];
+    }
+  }
+  return userDatabase;
+};
+
+//Inpsipired by https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+
+const generateRandomString = () => {
+  let tinyName = "";
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
+  let characterLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    tinyName += characters.charAt(Math.floor(Math.random() * characterLength));
+  } return tinyName;
+};
+
+
+module.exports = {findUserByEmail, urlsForUser, generateRandomString};
